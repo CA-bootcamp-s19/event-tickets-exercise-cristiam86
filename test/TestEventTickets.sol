31720c1b9e360f2102ad5f05f3851cce43e29ea4
Cristiam <cristiam@iamtech.tech>
2019-07-13 11:43:56 +0200

    Solidity test for events tickets V1

--- test/TestEventTickets.sol
+++ test/TestEventTickets.sol
@@ -7,17 +7,13 @@ import "./Proxy.sol";
 
 contract TestEventTickets {
   uint public initialBalance = 1 ether;
-  string description = 'book';
+  string description = 'my event';
   string url = 'website';
   uint totalTickets = 10;
   uint TICKET_PRICE = 100 wei;
 
-  // address buyer = 0xCdABc2bcb262A2aaDdD01de1f7710C6E2F50AC12;
-
   EventTickets public eventTickets;
   Proxy public ticketsBuyer;
-
-  event ConsoleLog(bytes);
 
   function() external payable {}
 
@@ -26,26 +22,62 @@ contract TestEventTickets {
     // Contract to test
     eventTickets = new EventTickets(description, url, totalTickets);
     ticketsBuyer = new Proxy(eventTickets);
-    address(ticketsBuyer).transfer(500 wei);
+    address(ticketsBuyer).transfer(100 wei);
+    ticketsBuyer.buyTickets(100 wei, 1);
+  }
 
-    (bool success, bytes memory ticketsBought) = ticketsBuyer.buyTickets(500 wei, 5);
-    emit ConsoleLog(ticketsBought);
-    Assert.isTrue(success, "Buyer should have bought 5 tickets");
+  function testBuyTickets() public {
+    address(ticketsBuyer).transfer(500 wei);
+    (bool success, uint ticketsBought) = ticketsBuyer.buyTickets(500 wei, 5);
+    Assert.isTrue(success, "Buyer should have completed the purchase");
+    Assert.isTrue(ticketsBought == 5, "Buyer should have bought 5 tickets");
   }
 
   function testReadEvent() public  {
-    (string memory eventDescription, string memory eventUrl, uint eventTotalTickets, uint eventSales, bool eventIsOpen) = eventTickets.readEvent();
-    Assert.isTrue(keccak256(abi.encodePacked(eventDescription)) == keccak256(abi.encodePacked(description)), "event not initialized properly");
-    Assert.isTrue(keccak256(abi.encodePacked(eventUrl)) == keccak256(abi.encodePacked(url)), "event not initialized properly");
-    Assert.isTrue(eventTotalTickets == totalTickets, "event not initialized properly");
-    Assert.isTrue(eventSales == 0, "event not initialized properly");
-    Assert.isTrue(eventIsOpen == true, "event not initialized properly");
+    ( string memory eventDescription,
+      string memory eventUrl,
+      uint eventTotalTickets,
+      uint eventSales,
+      bool eventIsOpen
+      ) = eventTickets.readEvent();
+    Assert.isTrue(
+      keccak256(abi.encodePacked(eventDescription)) == keccak256(abi.encodePacked(description)),
+      "event description not initialized properly"
+    );
+    Assert.isTrue(keccak256(abi.encodePacked(eventUrl)) == keccak256(abi.encodePacked(url)), "event url not initialized properly");
+    Assert.isTrue(eventTotalTickets == totalTickets, "event totalTickets not initialized properly");
+    Assert.isTrue(eventIsOpen == true, "event isOpen not initialized properly");
+    Assert.isTrue(eventSales == 1, "event sales not initialized properly");
   }
 
   function testGetBuyerTicketCount() public {
+    uint beforeBuyTickets = eventTickets.getBuyerTicketCount(address(ticketsBuyer));
+    Assert.isTrue(beforeBuyTickets == 1, "Buyer should have bought 1 ticket");
+
+    address(ticketsBuyer).transfer(500 wei);
+    ticketsBuyer.buyTickets(500 wei, 5);
     uint afterBuyTickets = eventTickets.getBuyerTicketCount(address(ticketsBuyer));
-    Assert.isTrue(afterBuyTickets == 5, "Buyer should have bought 5 tickets");
+    Assert.isTrue(afterBuyTickets == 6, "Buyer should have bought 6 tickets");
   }
 
+  function testGetRefund() public {
+    uint ticketsBeforeRefund = eventTickets.getBuyerTicketCount(address(ticketsBuyer));
+    ( , , , uint salesBeforeRefund, ) = eventTickets.readEvent();
 
+    bool success = ticketsBuyer.getRefund();
+    Assert.isTrue(success, "Tickets should have been refunded");
+
+    ( , , , uint salesAfterRefund, ) = eventTickets.readEvent();
+    Assert.isTrue(salesAfterRefund == salesBeforeRefund - ticketsBeforeRefund, 'Tickets have not been refunded');
+  }
+
+  function testEndSale() public {
+    eventTickets.endSale();
+    ( , , , , bool eventIsOpen) = eventTickets.readEvent();
+    Assert.isFalse(eventIsOpen, 'Event should be closed');
+
+    address(ticketsBuyer).transfer(500 wei);
+    (bool success, ) = ticketsBuyer.buyTickets(500 wei, 5);
+    Assert.isFalse(success, "Tickets should't been bought when event is closed");
+  }
 }
